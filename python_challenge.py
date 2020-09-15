27e623859140012242d2112c502b95d4cea3ad22
iam-regina <69836954+iam-regina@users.noreply.github.com>
2020-09-15 14:16:06 +0800

    Update python_challenge.py

--- python_challenge.py
+++ python_challenge.py
@@ -1,4 +1,4 @@
 ###Try these out!!! its really easy
-#find the error here!  don't be fooled by the unfamiliar code!
+#find the only error here!  don't be fooled by the unfamiliar code!
             
 while True print('Hello world')
