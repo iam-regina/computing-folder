703be71f4fa3c25db4fda8405410db8483443ba2
iam-regina <69836954+iam-regina@users.noreply.github.com>
2020-09-21 17:46:50 +0800

    Update python_challenge.py

--- python_challenge.py
+++ python_challenge.py
@@ -1,14 +1,6 @@
 ###Try these out!!! its really easy
-#find the only error here!  don't be fooled by the unfamiliar code!
+#find the only error here!
             
-While true:
-  print("Hello World!")
 
-# or for micro bit
-
-from microbit import*
-
-while True:
-  display.scroll('Hello World'!)
-
+print "hello"
 
